914e948e4808349627f79c1da549cdd4e58e1cb1
EmmanuelCioco <wearethedreamersoftommorrow@gmail.com>
2025-02-13 20:00:53 +0300

    Add files via upload
    this code checks if a number is odd or even

---
+++ IF_CONDITIONAL.py
@@ -0,0 +1,8 @@
+number=int(input("write a number: "))
+#if condition
+def if_conditional():
+    if number % 2 == 0:
+        print(f"{number} is an even number")
+    else:
+        print(f"{number} is an odd number")
+if_conditional()
