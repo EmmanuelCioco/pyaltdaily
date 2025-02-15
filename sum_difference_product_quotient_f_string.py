44881db985984724dccc120af0d962c2c992b0ba
EmmanuelCioco <wearethedreamersoftommorrow@gmail.com>
2025-02-15 13:04:51 +0300

    Add files via upload

---
+++ sum_difference_product_quotient_f_string.py
@@ -0,0 +1,10 @@
+def funct(a,b):
+    summ=a+b
+    print(f"The sum of {a} and {b} is",summ)
+    difference=(a+b)
+    print(f"The difference of {a} amd {b} is",difference)
+    product=(a*b)
+    print(f"The product of {a} amd {b} is",product)
+    quotient=(a/b)
+    print(f"The quotient of {a} amd {b} is",quotient)
+funct(1,4)
