d3f2d1acdb97b4625eca5b1fe9175c39f41a33f4
EmmanuelCioco <wearethedreamersoftommorrow@gmail.com>
2025-02-16 17:17:43 +0300

    Add files via upload
    This is the process of converting degrees Celsius to Fahrenheit

---
+++ convert_degrees_to_fahrenheit.py
@@ -0,0 +1,8 @@
+def celsius_to_fahrenheit(celsius):
+    """Converts Celsius to Fahrenheit and returns the result."""
+    fahrenheit = (9/5) * celsius + 32
+    return fahrenheit  # Return instead of print
+#entering the degrees to convert
+temp_in_f = celsius_to_fahrenheit(34)
+print(temp_in_f)
+
