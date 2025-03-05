32621232dd4fe8597c357b51535f4bf27d8aac83
EmmanuelCioco <wearethedreamersoftommorrow@gmail.com>
2025-03-05 23:57:04 +0300

    this is a file that tries to describe how to use classes in python

---
+++ myclass4m.py
@@ -0,0 +1,10 @@
+Class class:
+  def --init--(self,studentsName,grade):
+    self.studentsName=studentsName
+    self.grade=grade
+  def performane(self,grade):
+    if grade>=60:
+      print("exelled in exam")
+    else:
+      print("unfortunately failed")
+    
