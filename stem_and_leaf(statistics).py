888101a727e301d33f90d0efea6c5a98f262cf15
EmmanuelCioco <wearethedreamersoftommorrow@gmail.com>
2025-03-11 23:26:35 +0300

    sort the values ascending order

--- stem_and_leaf(statistics).py
+++ stem_and_leaf(statistics).py
@@ -1,18 +1,5 @@
 values=[3, 8, 12, 19, 24, 27, 31, 35, 42, 46, 53, 57, 61, 75, 89]
 def arrangement_of_values():
-  c=0
-  while True:
-      try:
-          m=values[c]
-          n=values[c+1]
-      except IndexError:
-          break
-      if m>n:
-          K=m
-          m=n
-          values[c]=m
-          n=K
-          values[c+1]=n
-      c+=1
+  values=values.sort()
 def length_of_last_values():
-  
+  x=len(str(values[-1]))
