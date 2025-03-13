5ce61abfa2d448f453f8150e579b35e1e1b664f5
EmmanuelCioco <wearethedreamersoftommorrow@gmail.com>
2025-03-13 17:22:21 +0300

    at least progressive change

--- stem_and_leaf(statistics).py
+++ stem_and_leaf(statistics).py
@@ -1,5 +1,12 @@
 values=[3, 8, 12, 19, 24, 27, 31, 35, 42, 46, 53, 57, 61, 75, 89]
-def arrangement_of_values():
-  values=values.sort()
-def length_of_last_values():
-  x=len(str(values[-1]))
+values=values.sort()
+arrangement_of_values()
+stem=[]
+leaf=[]
+stemleaf_dict{}
+for i in values:
+  i=str(i)
+  length=len(i)
+  if length==1:
+    stem=stem.append(0)
+    leaf=leaf.append(i)
